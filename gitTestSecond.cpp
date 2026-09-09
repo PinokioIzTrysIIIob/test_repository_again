9a2fe8673da4a5bad1701894c4be759d398e5a0c
C++Enjoyer <nikita.buz09@gmail.com>
2026-09-09 21:25:24 +0300

    You're now happy to be a fish :D (just useless fish code, ignore it

--- gitTestSecond.cpp
+++ gitTestSecond.cpp
@@ -8,9 +8,11 @@ int main()
     if(a == 3)  
     {
         std::cout << "fih" << std::endl;
+        std::cout << "YAY, IM A FIH!!!! :D";
     } else {
         std::cout << "I'm a fish" << std::endl;
+        std::cout << "YAY, IM A FISH!!! :D";
     }
-    
+
     return 0;
 }
