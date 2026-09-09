36c39e6c11f859ea3439775557ee17bfdff8a3f6
Flyaga <nikita.buz09@gmail.com>
2026-09-09 20:05:42 +0300

    Fih file, dont take it seriously :D

---
+++ gitTestSecond.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    int a;
+    std::cin >> a;
+    if(a == 3)  
+    {
+        std::cout << "fih" << std::endl;
+    } else {
+        std::cout << "I'm a fish" << std::endl;
+    }
+    
+    return 0;
+}
